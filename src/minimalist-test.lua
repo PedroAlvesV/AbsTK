63a59b51399142ac5ae6a98b5589a9676483cfb6
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-10-28 12:20:06 -0200

    Add files via upload

--- src/minimalist-test.lua
+++ src/minimalist-test.lua
@@ -1,79 +1,10 @@
-local lgi = require 'lgi'
-local Gtk = lgi.require('Gtk')
-local curses = require 'curses' 
+local abstk = require 'abstk'
 
-local GtkClass, CursesClass
+-- if mode is nil, then autodetect
+abstk.set_mode(...)
 
-GtkClass = {
-  new = function(title, w, h)
-    local self = {
-      window = Gtk.Window {
-        title = title,
-        default_width = w,
-        default_height = h,
-        on_destroy = Gtk.main_quit
-      }
-    }
-    local mt = {
-      __index = GtkClass,
-    }
-    setmetatable(self, mt)
-    return self
-  end,
-  
-  vbox = Gtk.VBox(),
-  
-  addLabel = function(self, label)
-    self.vbox:pack_start(Gtk.Label { label = label }, true, true, 0)
-  end,
-
-  run = function(self)
-    self.window:add(self.vbox)
-    self.window:show_all()
-    Gtk.main()
-  end,
-}
-
-CursesClass = {
-  new = function(title)
-    curses.initscr()
-    local self = {
-      stdscr = curses.stdscr()
-    }
-    local mt = {
-       __index = CursesClass,
-    }
-    setmetatable(self, mt)
-    self:addLabel('AbsTk Minimalist Test')
-    return self
-  end,
-  
-  altLine = 0,
-  
-  addLabel = function(self, label)
-    self.stdscr:mvaddstr(self.altLine,0,label)
-    self.altLine = self.altLine + 2
-  end,
-
-  run = function(self)
-    self.stdscr:refresh()
-    local c = self.stdscr:getch()
-    curses.endwin()
-  end,
-}
-
-function main()
-  local s = io.read()
-  local scr
-  if s == 'cur' then
-    scr = CursesClass.new('AbsTk Minimalist Test')
-  elseif s == 'gtk' then
-    scr = GtkClass.new('AbsTk Minimalist Test', 400, 300)
-  end
-  scr:addLabel('Parameter 1:\t\t1234')
-  scr:addLabel('Parameter 2:\t\tABCD')
-  scr:addLabel('Parameter 3:\t\tWXYZ')
-  scr:run()
-end
-
-main()
+local scr = abstk.new_screen("AbsTK Minimalist Test")
+scr:add_label('Parameter 1:\t\t1234')
+scr:add_label('Parameter 2:\t\tABCD')
+scr:add_label('Parameter 3:\t\tWXYZ')
+scr:run()
