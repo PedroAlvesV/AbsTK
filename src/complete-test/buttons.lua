acbf62eb185cc88c4763a15c1e04e2c00c051043
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-11-04 12:02:23 -0200

    Add files via upload

--- src/complete-test/buttons.lua
+++ src/complete-test/buttons.lua
@@ -3,8 +3,11 @@ local abstk = require 'abstk'
 abstk.set_mode(...)
 
 local scr = abstk.new_screen("AbsTK Complete Test - Buttons Module")
-scr:create_button_box({'A', 'B', 'C', 'D'})  -- https://developer.gnome.org/gtk3/stable/; http://equipe.nce.ufrj.br/adriano/c/apostila/gtk/html/tutorial.html
-local t = {
+scr:add_label('Simple Buttons')
+scr:add_button('Button1')
+scr:add_button('Button2')
+scr:create_button_box('Simple ButtonBox', {'A', 'B', 'C', 'D'})  -- https://developer.gnome.org/gtk3/stable/;             
+local t = {                                                      -- http://equipe.nce.ufrj.br/adriano/c/apostila/gtk/html/tutorial.html
   { name = "Parent1",
   "Leaf1", "Leaf2", "Leaf3"}, 
   { name = "Parent2",
---
+++ src/complete-test/images.lua
@@ -0,0 +1,8 @@
+local abstk = require 'abstk'
+
+abstk.set_mode(...)
+
+local scr = abstk.new_screen("AbsTK Complete Test - Images Module")
+scr:add_image('imgs/lua.png', 100, 100)
+scr:add_image('imgs/batman.png', 256, 192)
+scr:run()
