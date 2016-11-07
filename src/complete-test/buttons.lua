e7c37614fb85ffa9b28929b8023325cfda9fffb5
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-11-07 12:12:25 -0200

    Add files via upload

--- src/complete-test/buttons.lua
+++ src/complete-test/buttons.lua
@@ -3,9 +3,12 @@ local abstk = require 'abstk'
 abstk.set_mode(...)
 
 local scr = abstk.new_screen("AbsTK Complete Test - Buttons Module")
+
 scr:add_label('Simple Buttons')
+
 scr:add_button('Button1')
 scr:add_button('Button2')
+
 scr:create_button_box('Simple ButtonBox', {'A', 'B', 'C', 'D'})  -- https://developer.gnome.org/gtk3/stable/;             
 local t = {                                                      -- http://equipe.nce.ufrj.br/adriano/c/apostila/gtk/html/tutorial.html
   { name = "Parent1",
@@ -15,6 +18,8 @@ local t = {                                                      -- http://equip
   { name = "Parent3",
   "Leaf7", "Leaf8", "Leaf9"},
 }
+
 scr:create_combobox({'Label1', 'Label2', 'Label3'}, 'SIMPLE')
 scr:create_combobox(t, 'TREE')
+
 scr:run()
