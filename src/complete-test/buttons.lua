a765a83a871fc42e411a419fb05cbed131e560fc
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-11-08 11:55:06 -0200

    Now the buttons alignment can be specified
    In buttonboxes the alignment of the buttons is now specified as spread, start, edge and end, with a second parameter in the 'create_button_box' method.

--- src/complete-test/buttons.lua
+++ src/complete-test/buttons.lua
@@ -5,12 +5,17 @@ abstk.set_mode(...)
 local scr = abstk.new_screen("AbsTK Complete Test - Buttons Module")
 
 scr:add_label('Simple Buttons')
-
 scr:add_button('Button1')
 scr:add_button('Button2')
 
-scr:create_button_box('Simple ButtonBox', {'A', 'B', 'C', 'D'})  -- https://developer.gnome.org/gtk3/stable/;             
-local t = {                                                      -- http://equipe.nce.ufrj.br/adriano/c/apostila/gtk/html/tutorial.html
+scr:add_label('ButtonBox')
+scr:create_button_box({'A', 'B', 'C', 'D'}, 'SPREAD')  -- https://developer.gnome.org/gtk3/stable/;             
+                                                       -- http://equipe.nce.ufrj.br/adriano/c/apostila/gtk/html/tutorial.html
+
+scr:add_label('ComboBox (Simple)')
+scr:create_combobox({'Label1', 'Label2', 'Label3'}, 'SIMPLE')
+
+local t = {                                                      
   { name = "Parent1",
   "Leaf1", "Leaf2", "Leaf3"}, 
   { name = "Parent2",
@@ -18,8 +23,7 @@ local t = {                                                      -- http://equip
   { name = "Parent3",
   "Leaf7", "Leaf8", "Leaf9"},
 }
-
-scr:create_combobox({'Label1', 'Label2', 'Label3'}, 'SIMPLE')
+scr:add_label('ComboBox (Tree)')
 scr:create_combobox(t, 'TREE')
 
 scr:run()
