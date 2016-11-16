ff28d81b15987fcce6ee99581655dedf9f1f8c94
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-11-16 11:07:15 -0200

    Width and height are fields of a table

--- src/complete-test/images.lua
+++ src/complete-test/images.lua
@@ -4,7 +4,7 @@ abstk.set_mode(...)
 
 local scr = abstk.new_screen("AbsTK Complete Test - Images Module")
 
-scr:add_image('imgs/lua.png', 100, 100)
-scr:add_image('imgs/batman.png', 256, 192)
+scr:add_image('imgs/lua.png')
+scr:add_image('imgs/batman.png', {512, 384})
 
 scr:run()
