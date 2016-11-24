73e728a288f530312fd0166cc58e27baf2b2d61f
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-11-24 11:38:01 -0200

    Added scrolled lists to represent large checklists
    If a checklist has more than 9 elements, it gets into a scrolled list.

--- src/complete-test/lists.lua
+++ src/complete-test/lists.lua
@@ -10,10 +10,10 @@ scr:create_checklist('chklist1', {'a', 'b', 'c'})
 scr:add_label('label2', 'RadioButtons')
 scr:create_radiolist('rdlist', {'x', 'y', 'z'})
 
-scr:add_label('label3', 'Grid')
-scr:create_list('list', {'q', 'w', 'e', 'r', 't', 'y'})
+scr:add_label('label3', 'CheckList (if greater than 3 and less than 10, turns into grid)')
+scr:create_checklist('chklist2', {'q', 'w', 'e', 'r', 't', 'y'})
 
-scr:add_label('label4', 'CheckList (if larger than 3, turns into grid)')
-scr:create_checklist('chklist2', {'1', '2', '3', '4'})
+scr:add_label('label4', 'CheckList (if larger than 10, turns into scrolled list)')
+scr:create_checklist('chklist3', {'01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'})
 
 scr:run()
