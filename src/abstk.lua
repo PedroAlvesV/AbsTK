221883195f99283fcdca5c6d9bafcccd6f17454b
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2016-12-14 12:17:22 -0200

    Updating abstk.lua
    Made obj a metatable for self.
    Also, redesigned the way a password input is called. Client doesn't pass the boolean anymore, it justs call the proper function.
    Unfortunately, the getters and setters return nothing but nil to client, unless new_screen(...) explicitly returns obj. Must find a way to fix this.

--- src/abstk.lua
+++ src/abstk.lua
@@ -32,20 +32,23 @@ function abstk.new_screen(title, w, h)
     add_label = function(self, id, label, tooltip, callback)
       obj:add_label(id, label, tooltip, callback)
     end,
-    add_button = function(self, id, label, default_value, tooltip, callback)
-      obj:add_button(id, label, default_value, tooltip, callback)
+    add_button = function(self, id, label, tooltip, callback)
+      obj:add_button(id, label, tooltip, callback)
     end,
-    create_button_box = function(self, id, labels, layout, default_value, tooltip, callback)
-      obj:create_button_box(id, labels, layout, default_value, tooltip, callback)
+    create_button_box = function(self, id, labels, layout, tooltip, callback)
+      obj:create_button_box(id, labels, layout, tooltip, callback)
     end,
     create_combobox = function(self, id, labels, default_value, tooltip, callback)
       obj:create_combobox(id, labels, default_value, tooltip, callback)
     end,
-    add_image = function(self, id, path, dimensions, tooltip, callback)
-      obj:add_image(id, path, dimensions, tooltip, callback)
+    add_image = function(self, id, path, dimensions, tooltip)
+      obj:add_image(id, path, dimensions, tooltip)
     end,
-    add_text_input = function(self, id, title, is_password, default_value, tooltip, callback)
-      obj:add_text_input(id, title, is_password, default_value, tooltip, callback)
+    add_text_input = function(self, id, title, default_value, tooltip, callback)
+      obj:add_text_input(id, title, true, default_value, tooltip, callback)
+    end,
+    add_password = function(self, id, title, default_value, tooltip, callback)
+      obj:add_text_input(id, title, false, default_value, tooltip, callback)
     end,
     add_textbox = function(self, id, default_value, tooltip, callback)
       obj:add_textbox(id, default_value, tooltip, callback)
@@ -56,14 +59,30 @@ function abstk.new_screen(title, w, h)
     create_radiolist = function(self, id, list, default_value, tooltip, callback)
       obj:create_radiolist(id, list, default_value, tooltip, callback)
     end,
-    create_list = function(self, id, list, default_value, tooltip, callback)
-      obj:create_list(id, list, default_value, tooltip, callback)
+    create_list = function(self, id, list, tooltip, callback)
+      obj:create_list(id, list, tooltip, callback)
+    end,
+    show_message_box = function(self, id, message, buttons)
+      obj:show_message_box(id, message, buttons)
+    end,
+    set_enabled = function(self, id, bool, ...)
+      obj:set_enabled(id, bool, ...)
+    end,
+    set_value = function(self, id, value, index)
+      obj:set_value(id, value, index)
+    end,
+    get_value = function(self, id, index)
+      obj:get_value(id, index)
     end,
     run = function(self)
       obj:run()
     end,
   }
-  return obj
+  local mt = {
+    __index = obj,
+  }
+  setmetatable(self, mt)
+  return self
 end
 
 function abstk.new_wizard(title, w, h)
