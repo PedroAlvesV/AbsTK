bbb1615a3e1a9df34ab1620b8144fb62c5e486f0
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-01-13 18:16:31 -0200

    Beginning of AbsCurses

--- src/abstk/AbsCurses.lua
+++ src/abstk/AbsCurses.lua
@@ -1,18 +1,54 @@
 local AbsCurses = {}
 
 local curses = require 'curses' -- http://www.pjb.com.au/comp/lua/lcurses.html
-
+-- https://github.com/jballanc/playgo/blob/master/doc/curses-examples/lcurses-test.lua
 local Screen = {}
 local Wizard = {}
 
+local AbsCursesLabel = {}
+local AbsCursesButton = {}
+local AbsCursesButtonBox = {}
+local AbsCursesComboBox = {}
+local AbsCursesTextInput = {}
+local AbsCursesTextBox = {}
+local AbsCursesGrid = {}
+local AbsCursesCheckList = {}
+local AbsCursesRadioList = {}
+local AbsCursesList = {}
+
+local max_x, max_y
+local colors = {}
+local actions = {
+  PASSTHROUGH = 0,
+  HANDLED = -2,
+  PREVIOUS = -1,
+  NEXT = 1,
+  FOCUS_ON_BUTTONS = 10
+}
+local keys = {
+  TAB = 9,
+  ENTER = 10,
+  ESC = 27,
+  SPACE = 32,
+}
+local buttons = {
+  PREVIOUS = 0,
+  NEXT = 1,
+  QUIT = 2,
+  LAST = 2,
+}
+
+local function str_attr(str, attr)
+  local ch = curses.new_chstr(#str)
+  ch:set_str(0, str, attr)
+  return ch
+end
+
 function AbsCurses.new_screen(title)
-  curses.initscr()
-  curses.cbreak()
-  curses.echo(false)
-  curses.nl(false)
   local self = {
-    stdscr = curses.stdscr(),
-    alt_line = 0,
+    title = title,
+    widgets = {},
+    focus = 0,
   }
   local mt = {
     __index = Screen,
@@ -32,43 +68,105 @@ function AbsCurses.new_wizard(title)
   return self
 end
 
-function Screen:add_label(id, label)
+function AbsCursesLabel.new(label)
+  local self = {
+    height = 1,
+    label = label,
+    focusable = false,
+  }
+  return setmetatable(self, { __index = AbsCursesLabel })
+end
 
+function AbsCursesLabel:draw(drawable, x, y)
+  drawable:mvaddstr(y, x, self.label)
+end
+
+function AbsCursesButton.new(label)
+  local self = {
+    height = 1,
+    label = " "..label.." ",
+    focusable = true,
+  }
+  return setmetatable(self, { __index = AbsCursesButton })
+end
+
+function AbsCursesButton:draw(drawable, x, y, focus)
+  drawable:mvaddstr(y, x, self.label, colors.button)
+  if focus then
+    local left, right = ">", "<"
+    drawable:mvaddstr(y, x-1, left)
+    drawable:mvaddstr(y, x+string.len(self.label), right)
+  end
+end
+
+function AbsCursesButton:process_key(key)
+  if (key == keys.ENTER or key == keys.SPACE) and self.enabled then
+    self.runCallback()
+  elseif key == curses.KEY_LEFT or key == curses.KEY_RIGHT then
+    return actions.FOCUS_ON_BUTTONS
+  elseif key == keys.TAB or key == curses.KEY_DOWN then
+    return actions.NEXT
+  elseif key == curses.KEY_UP then
+    return actions.PREVIOUS
+  end
+  return actions.PASSTHROUGH
+end
+
+function AbsCursesButtonBox.new(labels)
+  local self = {
+    height = 1,
+    labels = labels,
+    focusable = true,
+  }
+  return setmetatable(self, { __index = AbsCursesButtonBox })
+end
+
+function AbsCursesButtonBox:draw(drawable, x, y, focus)
+  for _, label in ipairs(self.labels) do
+    drawable:mvaddstr(y, x, " "..label.." ", colors.button)
+    x = x + 7
+  end
+  if focus then
+    -- TODO subfocus
+    local left, right = ">", "<"
+    drawable.addstr(y, x-1, left)
+    drawable.addstr(y, x+string.len(self.label), right)
+  end
+end
+
+function Screen:add_label(id, label)
   local item = {
     id = id,
     type = 'LABEL',
-    widget = widget,
+    widget = AbsCursesLabel.new(label),
   }
   table.insert(self.widgets, item)
 end
 
 function Screen:add_button(id, label, tooltip, callback)
-
   local item = {
     id = id,
     type = 'BUTTON',
-    widget = widget,
+    widget = AbsCursesButton.new(label),
   }
   table.insert(self.widgets, item)
 end
 
 function Screen:create_button_box(id, labels, tooltip, callback)
-
   local item = {
     id = id,
     type = 'BUTTON_BOX',
-    widget = widget,
+    widget = AbsCursesButtonBox.new(labels),
   }
   table.insert(self.widgets, item)
 end
 
 function Screen:create_combobox(id, labels, default_value, tooltip, callback)
-
   local item = {
     id = id,
     type = 'COMBOBOX',
     labels = labels,
-    widget = widget,
+    widget = AbsCursesComboBox.new(labels),
   }
   table.insert(self.widgets, item)
 end
@@ -78,51 +176,54 @@ function Screen:add_image(id, path, dimensions, tooltip)
 end
 
 function Screen:add_text_input(id, label, visibility, default_value, tooltip, callback)
-
   local item = {
     id = id,
     type = 'TEXT_INPUT',
-    widget = widget,
+    widget = AbsCursesTextInput.new(label, visibility),
   }
   table.insert(self.widgets, item)
 end
 
 function Screen:add_textbox(id, default_value, tooltip, callback)
-
   local item = {
     id = id,
     type = 'TEXTBOX',
-    widget = widget,
+    widget = AbsCursesTextBox.new(),
   }
   table.insert(self.widgets, item)
 end
 
 function Screen:create_checklist(id, list, default_value, tooltip, callback)
-
-  local item = {
-    id = id,
-    type = 'GRID',
-    widget = widget,
-  }
+  if #list < 4 then
+    local item = {
+      id = id,
+      type = 'CHECKLIST',
+      widget = AbsCursesCheckList.new(list),
+    }
+  else
+    local item = {
+      id = id,
+      type = 'GRID',
+      widget = AbsCursesGrid.new(list),
+    }
+  end
   table.insert(self.widgets, item)
 end
 
 function Screen:create_radiolist(id, list, default_value, tooltip, callback)
-
   local item = {
     id = id,
     type = 'RADIOLIST',
-    widget = widget,
+    widget = AbsCursesRadioList.new(list),
   }
   table.insert(self.widgets, item)
 end
 
 function Screen:create_list(id, list, tooltip, callback)
-
   local item = {
     id = id,
     type = 'LIST',
-    widget = widget,
+    widget = AbsCursesList.new(list),
   }
   table.insert(self.widgets, item)
 end
@@ -210,8 +311,65 @@ function Screen:get_value(id, index)
   end
 end
 
-function Screen:run()
+local function init_curses()
+  local stdscr = curses.initscr()
+  curses.cbreak()
+  curses.echo(false)
+  curses.nl(false)
+  max_y, max_x = stdscr:getmaxyx()
+  max_x = math.min(max_x, 127)
+  -- max_y = math.min(max_y, 24)
+  curses.start_color()
+  curses.use_default_colors()
+  pcall(curses.init_pair, 1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+  pcall(curses.init_pair, 2, curses.COLOR_BLACK, curses.COLOR_BLUE)
+  pcall(curses.init_pair, 3, curses.COLOR_YELLOW, curses.COLOR_BLUE)
+  pcall(curses.init_pair, 4, curses.COLOR_BLACK, curses.COLOR_WHITE)
+  pcall(curses.init_pair, 5, curses.COLOR_BLACK, curses.COLOR_CYAN)
+  pcall(curses.init_pair, 6, curses.COLOR_CYAN, curses.COLOR_BLUE)
+  colors.default = curses.color_pair(1)
+  colors.disabled = curses.color_pair(2) + curses.A_BOLD
+  colors.title = curses.color_pair(3) + curses.A_BOLD
+  colors.button = curses.color_pair(4)
+  colors.widget = curses.color_pair(5)
+  colors.widget_disabled = curses.color_pair(5) + curses.A_BOLD
+  colors.current = curses.color_pair(6) + curses.A_BOLD
+  stdscr:clear()
+  stdscr:wbkgd(str_attr(" ", colors.default)) -- TODO fix colors
+  stdscr:attrset(colors.default)
+  return stdscr
+end
 
+function Screen:run()
+  local stdscr = init_curses()
+  self.pad = curses.newpad(max_x-2 + 100, max_y-4 + 100)
+  stdscr:sub(max_y-1, max_x, 0, 0):box(0, 0)
+  stdscr:refresh()
+  for i, item in ipairs(self.widgets) do
+    if item.widget.focusable then
+      self.focus = i
+      break
+    end
+  end
+  while true do
+    --local title = AbsCursesLabel.new(self.title)
+    --title:draw(stdscr, 3, 2)
+    self.pad:mvaddstr(1, 2, self.title)
+    self.pad:prefresh(0, 0, 1, 1, max_y-5, max_x-2)
+    local y = 4
+    for i, item in ipairs(self.widgets) do
+      if i == self.focus then
+        stdscr:mvaddstr(y, 3, ">")
+      end
+      -- TODO drawable pad (curses.newpad()), como uma div para tratar scroll
+      item.widget:draw(stdscr, 5, y)
+      y = y + item.widget.height + 1
+    end
+    local action = self.process_key(stdscr:getch())
+    -- TODO process key
+    break
+  end
+  -- done_curses()
 end
 
 function Wizard:add_page(id, screen, page_type)
--- src/minimalist-test/minimalist-test.lua
+++ src/minimalist-test/minimalist-test.lua
@@ -2,8 +2,10 @@ local abstk = require 'abstk'
 
 abstk.set_mode(...)
 
-local scr = abstk.new_screen('screen', "AbsTK Minimalist Test")
+local scr = abstk.new_screen("AbsTK Minimalist Test")
 scr:add_label('label1', 'Parameter 1:\t\t1234')
 scr:add_label('label2', 'Parameter 2:\t\tABCD')
 scr:add_label('label3', 'Parameter 3:\t\tWXYZ')
+scr:add_button('button', 'Test Button')
+scr:create_button_box('bbox', {'A', 'B', 'C', 'D'})
 scr:run()
