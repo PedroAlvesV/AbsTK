8078d00ab28c40af378117a5f3bde59bc72d0eac
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-02-14 10:35:12 -0200

    Updated some test modules

--- src/complete-test/complete-test.lua
+++ src/complete-test/complete-test.lua
@@ -15,22 +15,16 @@ local scr5 = abstk.new_screen("Text Input Module")
 -- Generic callbacks
 local bt_callback = function(id, label)
    print(label.." clicado.")
-   scr2:show_message_box('msgbox', label, 'OK')
+--   scr2:show_message_box('msgbox', label, 'OK')
 end
-local cbb_callback = function(id, value)
+local default_callback = function(id, value)
    print(id, value)
 end
 local chk_callback = function(id, value, index)
    print(scr4:get_value(id, index))
 end
-local rd_callback = function(id, value)
+local slct_callback = function(id, value)
    print(scr4:get_value(id))
-end
-local list_callback = function(id, value, index)
-   print(scr4:get_value(id, index))
-end
-local txt_callback = function(id, value)
-   print(id, value)
 end
 
 -- Fill the first screen
@@ -43,51 +37,58 @@ scr2:add_label('label1', 'Simple Buttons')
 scr2:add_button('bt1', 'Button1', "tooltip", bt_callback)
 scr2:add_button('bt2', 'Button2', nil, bt_callback)
 scr2:add_label('label2', 'ButtonBox')
-scr2:create_button_box('bbox', {'A', 'B', 'C', 'D'}, nil, bt_callback)
-scr2:add_label('label3', 'ComboBox')
-scr2:create_combobox('cbox', {'Label1', 'Label2', 'Label3'}, nil, cbb_callback)
+scr2:create_button_box('bbox', {'A', 'B', 'C', 'D'}, nil, {bt_callback, bt_callback, bt_callback, bt_callback})
+scr2:create_combobox('cbox', "ComboBox", {'Label1', 'Label2', 'Label3'}, nil, nil, default_callback)
 
 -- Fill the third screen
 scr3:add_image('lua_img', 'images/lua.png')
 scr3:add_image('batman_img', 'images/batman.png', {512, 384})
 
 -- Fill the fourth screen
-scr4:create_checklist('chklist1', "CheckBoxes (default construction)", {'a', 'b', 'c'}, nil, nil, chk_callback)
-scr4:create_checklist('chklist2', "CheckBoxes (constructed by passing values by default_value)", {'7', '8', '9'}, {true, false, true}, nil, chk_callback)
+scr4:add_checkbox('chkbox', "Test CheckBox", nil, nil, chk_callback)
+scr4:create_checklist('chklist1', "CheckList (short, default construction)", {'a', 'b', 'c'}, nil, nil, chk_callback)
+scr4:create_checklist('chklist2', "CheckList (short, constructed by passing values by default_value)", {'7', '8', '9'}, {true, false, true}, nil, chk_callback)
 local checklist_values = {
    {'z', false},
    {'x', true},
    {'c', true},
 }
-scr4:create_checklist('chklist3', "CheckBoxes (constructed by passing values in the elements table)", checklist_values, nil, nil, chk_callback)
-scr4:create_radiolist('rdlist1', "RadioButtons (default construction)", {'x', 'y', 'z'}, nil, nil, rd_callback)
-scr4:create_radiolist('rdlist2', "RadioButtons (constructed by passing index by default_value)", {'a', 's', 'd'}, 3, nil, rd_callback)
-local radiolist_values = {
+scr4:create_checklist('chklist3', "CheckList (short, constructed by passing values in the elements table)", checklist_values, nil, nil, chk_callback)
+local long_checklist = {
+   { "Item1", false },
+   { "Item2", true },
+   { "Item3", true },
+   { "Item4", false },
+   { "Item5", false },
+   { "Item6", false },
+   { "Item7", true },
+   { "Item8", false },
+   { "Item9", false },
+}
+scr4:create_checklist('chklist4', "CheckList (long, constructed by passing booleans in the elements table)", long_checklist , nil, nil, chk_callback)
+scr4:create_selector('selector1', "Selector (short, default construction)", {'x', 'y', 'z'}, nil, nil, slct_callback)
+scr4:create_selector('selector2', "Selector (short, constructed by passing index by default_value)", {'a', 's', 'd'}, 3, nil, slct_callback)
+local short_selector_table = {
    {'q', false},
    {'w', true},
    {'e', false},
 }
-scr4:create_radiolist('rdlist3', "RadioButtons (constructed by passing booleans in the elements table)", radiolist_values, nil, nil, rd_callback)
-local list = {
-   { "Item1", false },
-   { "Item2", true },
-   { "Item3", false },
-   { "Item4", false },
-   { "Item5", false },
-   { "Item6", false },
-   { "Item7", false },
-   { "Item8", false },
-   { "Item9", false },
-}
-scr4:create_list('chklist5', "List (constructed by passing booleans in the elements table)", list , nil, list_callback)
-scr4:create_list('chklist6', "List (constructed by passing just the labels)", {"Item10", "Item11", "Item12"} , nil, list_callback)
+scr4:create_selector('selector3', "Selector (short, constructed by passing booleans in the elements table)", short_selector_table, nil, nil, slct_callback)
+scr4:create_selector('selector4', "Selector (long, default construction)", {'v1', 'v2', 'v3', 'v4', 'v5', 'v6'}, nil, nil, slct_callback)
 
 -- Fill the fifth screen
-scr5:add_text_input('input1', 'Username', nil, nil, txt_callback)
-scr5:add_password('input2', 'Password', nil, nil, txt_callback)
-scr5:add_text_input('input3', nil, nil, nil, txt_callback)
-scr5:add_label('label', 'TextBox')
-scr5:add_textbox('box', nil, nil, nil, txt_callback)
+scr5:add_text_input('input1', 'Username', nil, nil, default_callback)
+scr5:add_password('input2', 'Password', nil, nil, default_callback)
+scr5:add_text_input('input3', nil, nil, nil, default_callback)
+local txt = ""
+local n = 100
+for i=1, n do
+   txt = txt..i
+   if i ~= n then
+      txt = txt.."\n"
+   end
+end
+scr5:add_textbox('tbox', "TextBox", txt, nil, default_callback)
 
 -- Add all screens to wizard
 wizard:add_page('screen1', scr1, 'INTRO')
--- src/minimalist-test/minimalist-test.lua
+++ src/minimalist-test/minimalist-test.lua
@@ -5,7 +5,7 @@ abstk.set_mode(...)
 local scr = abstk.new_screen("AbsTK Minimalist Test")
 
 local test_callback = function(id, value)
---   scr:set_enabled('bbox', false, 2)
+--   print(scr:get_value('tbox'))
 end
 
 scr:add_label('label1', 'Parameter 1:\t\t1234')
@@ -13,23 +13,31 @@ scr:add_label('label2', 'Parameter 2:\t\tABCD')
 scr:add_label('label3', 'Parameter 3:\t\tWXYZ')
 scr:add_button('button', "Test Button", "Button", test_callback)
 --scr:create_button_box('bbox', {"ABC", "DEF", "LOREM IPSUM", "G"}, {"Button1", "Button2", "Button3", "Button4"})
---scr:add_text_input('tinput', "Text Input:", "placeholder", "Entry")
---scr:add_password('tpassword', "Password Input:", "place", "Password Entry")
---scr:add_checkbox('chkbox', "Test CheckBox", true, "CheckBox")
---local l1 = {'a', 'b', 'c'}
---local l2 = {'d', 'e', 'f'}
---local l3 = {
---   {'g', false},
---   {'h', true},
---   {'i', false},
---}
---local l4 = {'x', 'y', 'z'}
+scr:add_text_input('tinput', "Text Input:", "placeholder", "Entry")
+scr:add_password('tpassword', "Password Input:", "place", "Password Entry")
+scr:add_checkbox('chkbox', "Test CheckBox", true, "CheckBox")
+local l1 = {'a', 'b', 'c'}
+local l2 = {'d', 'e', 'f', '1', '2', '3', '4', '5', '6'}
+local l3 = {
+   {'g', false},
+   {'h', true},
+   {'i', false},
+}
+local l4 = {'x', 'y', 'z'}
 --scr:create_checklist('chklist1', "Check1", l1,  nil, "Checkboxes group")
 --scr:create_checklist('chklist2', "Check2", l2, {true, true, false}, "Checkboxes group")
 --scr:create_checklist('chklist3', "Check3", l3, nil, "Checkboxes group")
---scr:create_checklist('chklist3', "Check4", l4, 3, "Checkboxes group")
---scr:create_radiolist('rd1', "Radio1", l1)
---scr:create_radiolist('rd2', "Radio2", l2, 3)
---scr:create_radiolist('rd3', "Radio3", l3)
-scr:add_textbox('tbox', "", "lorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\nlorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17")
+--scr:create_checklist('chklist4', "Check4", l4, 3, "Checkboxes group")
+--scr:create_selector('sl1', "Selector1", l1, nil, nil, test_callback)
+--scr:create_selector('sl2', "Selector2", l2, 3, nil, test_callback)
+--scr:create_selector('sl3', "Selector3", l3)
+local txt = ""
+local n = 100
+for i=1, n do
+   txt = txt..i
+   if i ~= n then
+      txt = txt.."\n"
+   end
+end
+scr:add_textbox('tbox', "TextBox", txt, "TextBox")
 scr:run()
