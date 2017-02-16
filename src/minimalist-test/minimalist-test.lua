e939dd8ef91a34f4a6d941649852e4fec141be02
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-02-16 20:15:34 -0200

    Minor changes in minimalist-test

--- src/minimalist-test/minimalist-test.lua
+++ src/minimalist-test/minimalist-test.lua
@@ -4,8 +4,8 @@ abstk.set_mode(...)
 
 local scr = abstk.new_screen("AbsTK Minimalist Test")
 
-local test_callback = function(id, value)
-   scr:set_enabled('button', false)
+local test_callback = function(id, value, ...)
+   print(id, value, ...)
 end
 
 scr:add_label('label1', 'Parameter 1:\t\t1234')
@@ -17,19 +17,20 @@ scr:add_text_input('tinput', "Text Input:", "placeholder", "Entry")
 scr:add_password('tpassword', "Password Input:", "place", "Password Entry")
 scr:add_checkbox('chkbox', "Test CheckBox", true, "CheckBox")
 local l1 = {'a', 'b', 'c'}
-local l2 = {'d', 'e', 'f'}
+local l2 = {'d', 'e', 'f', '1', '2', '3'}
 local l3 = {
    {'g', false},
    {'h', true},
    {'i', false},
 }
 local l4 = {'x', 'y', 'z'}
---scr:create_checklist('chklist1', "Check1", l1,  nil, "Checkboxes group")
---scr:create_checklist('chklist2', "Check2", l2, {true, true, false}, "Checkboxes group")
---scr:create_checklist('chklist3', "Check3", l3, nil, "Checkboxes group")
---scr:create_checklist('chklist3', "Check4", l4, 3, "Checkboxes group")
---scr:create_selector('sl1', "Selector1", l1)
---scr:create_selector('sl2', "Selector2", l2, 3)
---scr:create_selector('sl3', "Selector3", l3)
-scr:add_textbox('tbox', "", "lorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\nlorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17", "TextBox")
+scr:create_checklist('chklist1', "Check1", l1,  nil, "Checkboxes group")
+scr:create_checklist('chklist2', "Check2", l2, {true, true, false}, "Checkboxes group")
+scr:create_checklist('chklist3', "Check3", l3, nil, "Checkboxes group")
+scr:create_checklist('chklist3', "Check4", l4, 3, "Checkboxes group")
+scr:create_selector('sl1', "Selector1", l1)
+scr:create_selector('sl2', "Selector2", l2, 3)
+scr:create_selector('sl3', "Selector3", l3)
+scr:add_textbox('tbox', "TextBox", "lorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\nlorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17", "TextBox")
+--scr:add_textbox('tbox2', nil, "lorem\nipsum\n3\n4\n5\n6\n7\n8\n9\n10\n11", "TextBox")
 scr:run()
