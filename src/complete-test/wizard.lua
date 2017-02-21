56391b76ceb48c15f1560e00c83059571b236a52
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-02-21 17:40:36 -0300

    Improved wizard.lua test module
    With just an image on the second screen, it couldn't be properly tested on text-mode.

--- src/complete-test/wizard.lua
+++ src/complete-test/wizard.lua
@@ -7,7 +7,10 @@ local wizard = abstk.new_wizard("AbsTK Complete Test - Wizard", 400, 300)
 local scr1 = abstk.new_screen("Page 1")
 local scr2 = abstk.new_screen("Page 2")
 
-scr1:add_label('label', "Lorem Ipsum")
+scr1:add_label('label', "Test Label")
+scr1:add_button('button', "Lorem Ipsum")
+
+scr2:add_label('label', "It should show an image below me")
 scr2:add_image('image', 'imgs/batman.png', {512, 384})
 
 wizard:add_page('screen1', scr1, 'INTRO')
